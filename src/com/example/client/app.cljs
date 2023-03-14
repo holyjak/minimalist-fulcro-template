382ea86a504deeb13a4c6e4909c0a7d6c9f9df31
Jakub Holy <jakubholy@jakubholy.net>
2023-03-14 18:34:49 +0100

    Update deps, react -> 18

--- src/com/example/client/app.cljs
+++ src/com/example/client/app.cljs
@@ -1,6 +1,7 @@
 (ns com.example.client.app
   (:require
    [com.fulcrologic.fulcro.application :as app]
+   [com.fulcrologic.fulcro.react.version18 :refer [with-react18]]
    [com.fulcrologic.fulcro.networking.http-remote :as http]
    [edn-query-language.core :as eql]))
 
@@ -11,16 +12,18 @@
     (not= :call (:type ast)) ; skip mutations
     (update :children conj (eql/expr->ast :com.wsscode.pathom3.connect.runner/attribute-errors))))
 
-(defonce app (app/fulcro-app {:remotes {:remote (http/fulcro-http-remote {})}
-                              :global-eql-transform global-eql-transform
-                              :remote-error?
-                              (fn [result]
-                                (or
-                                 (app/default-remote-error? result)
-                                 (:com.wsscode.pathom3.connect.runner/attribute-errors (:body result))))
-                              :global-error-action
-                              (fn [{{:keys [body status-code error-text]} :result :as env}]
-                                (println "WARN: Remote call failed"
-                                         status-code
-                                         error-text
-                                         (:com.wsscode.pathom3.connect.runner/attribute-errors body)))}))
+(defonce app 
+  (-> (app/fulcro-app {:remotes {:remote (http/fulcro-http-remote {})}
+                       :global-eql-transform global-eql-transform
+                       :remote-error?
+                       (fn [result]
+                         (or
+                          (app/default-remote-error? result)
+                          (:com.wsscode.pathom3.connect.runner/attribute-errors (:body result))))
+                       :global-error-action
+                       (fn [{{:keys [body status-code error-text]} :result :as env}]
+                         (println "WARN: Remote call failed"
+                                  status-code
+                                  error-text
+                                  (:com.wsscode.pathom3.connect.runner/attribute-errors body)))})
+      (with-react18)))
